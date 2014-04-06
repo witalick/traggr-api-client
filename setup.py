374b6b8528188c36dea1f9e00689e14b07759a1c
Vitaliy Yakoviv <witalick@gmail.com>
2014-04-06 21:00:12 +0300

    Minor clean up

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from setuptools import setup
 
 __version__ = '0.2'
 
-setup(name = 'traggr-api-client',
-      packages = ['.'],
-      version = __version__,
-      description = 'API client for Test Results Aggregation system',
-      author = 'Vitaliy Yakoviv',
-      author_email = 'witalick@gmail.com',
-      url = 'https://github.com/witalick/traggr-api-client',
-      keywords = ['api', 'testing'],
-      classifiers = [],
+
+setup(name='traggr-api-client',
+      py_modules=['traggrcl'],
+      version=__version__,
+      description='API client for Test Results Aggregation system',
+      author='Vitaliy Yakoviv',
+      author_email='witalick@gmail.com',
+      url='https://github.com/witalick/traggr-api-client',
+      keywords=['api', 'testing'],
+      classifiers=[],
       install_requires=['requests'])
+
+
+# EOF

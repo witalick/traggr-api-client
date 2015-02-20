10b077f9cbdc6e6856a0463286bf6892d371aca8
vkhomchak <vkhomchak@llnw.com>
2015-02-20 15:29:11 +0200

    Added manaul tests methods

--- traggrcl.py
+++ traggrcl.py
@@ -15,15 +15,12 @@ REQUIRED_RESULT_KEYS = ('component', 'suite', 'test_id')
 class TRAggrAPIClient(object):
 
     def __init__(self, url):
-
         self.base_url = url
 
     def _compile_url(self, path):
-
         return '%s/%s' % (self.base_url.rstrip('/'), path.lstrip('/'))
 
     def ping(self):
-
         path = '/ping'
         url = self._compile_url(path)
         response = requests.get(url)
@@ -31,17 +28,14 @@ class TRAggrAPIClient(object):
         if response.status_code != 200:
             raise Exception('API does not respond.')
 
-    def post_results(self, project,
-                           sprint,
-                           results):
-
+    def post_results(self, project, sprint, results):
         headers = {'Content-Type': 'text/json'}
 
         # Validate results.
         for result in results:
             for required_key in REQUIRED_RESULT_KEYS:
                 if required_key not in result:
-                    raise Exception('Result record %s does not contain a required key "%s"' % (record, required_key))
+                    raise Exception('Result record %s does not contain a required key "%s"' % (result, required_key))
         # TODO: More validation.
 
         path = '/results/%s/%s' % (project, sprint)
@@ -53,6 +47,43 @@ class TRAggrAPIClient(object):
             raise Exception('Failed to post results. Code: %s. Text: %s' %
                             (response.status_code, response.text))
 
+    def post_manual_test(self, project, tests):
+        headers = {'Content-Type': 'text/json'}
+
+        # Validate results.
+        for test in tests:
+            for required_key in REQUIRED_RESULT_KEYS:
+                if required_key not in test:
+                    raise Exception('Result record %s does not contain a required key "%s"' % (test, required_key))
+        # TODO: More validation.
+
+        path = '/manual/%s' % project
+        url = self._compile_url(path)
+
+        response = requests.post(url, data=json.dumps(results), headers=headers)
+
+        if response.status_code != 200:
+            raise Exception('Failed to post results. Code: %s. Text: %s' %
+                            (response.status_code, response.text))
+
+    def post_manual_results(self, project, sprint, results):
+        headers = {'Content-Type': 'text/json'}
+
+        # Validate results.
+        for result in results:
+            for required_key in REQUIRED_RESULT_KEYS:
+                if required_key not in result:
+                    raise Exception('Result record %s does not contain a required key "%s"' % (result, required_key))
+        # TODO: More validation.
+
+        path = '/manual/results/%s/%s' % (project, sprint)
+        url = self._compile_url(path)
+
+        response = requests.post(url, data=json.dumps(results), headers=headers)
+
+        if response.status_code != 200:
+            raise Exception('Failed to post results. Code: %s. Text: %s' %
+                            (response.status_code, response.text))
 
 if __name__ == '__main__':
 
